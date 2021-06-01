51f0be8c3d70b5cfd063ee74637fae4a78621056
Jesús Millán <jmillan@vidaplatform.com>
2021-05-31 23:56:39 -0400

    :recycle:[REF] Restructure ppackages

--- app/url_mappings.go
+++ app/url_mappings.go
@@ -1,9 +1,15 @@
 package app
 
 import (
-	"github.com/jgmc3012/bookstore_users-api/controllers"
+	"github.com/jgmc3012/bookstore_users-api/controllers/ping"
+	"github.com/jgmc3012/bookstore_users-api/controllers/users"
 )
 
 func mapUrls() {
-	router.GET("/ping", controllers.Ping)
+	router.GET("/ping/", ping.Ping)
+
+	router.GET("/users/:user_id/", users.GetUser)
+	router.GET("/users/search/", users.SearchUser)
+	router.POST("/users/", users.CreateUser)
+
 }
