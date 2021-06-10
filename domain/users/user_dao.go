db91f79a78466ebd843dacbbd129afe4329d4638
Jesús Millán <jgmc3012@gmail.com>
2021-06-09 22:39:04 -0400

    :star:[ADD] Manage dates and add CreatedDate field

--- domain/users/user_dao.go
+++ domain/users/user_dao.go
@@ -3,6 +3,7 @@ package users
 import (
 	"fmt"
 
+	"github.com/jgmc3012/bookstore_users-api/utils/date_utils"
 	"github.com/jgmc3012/bookstore_users-api/utils/errors"
 )
 
@@ -26,6 +27,9 @@ func (user *User) Save() *errors.RestErr {
 		}
 		return errors.NewBadRequestError(fmt.Sprintf("user %d already exists", user.Id))
 	}
+
+	user.DateCreated = date_utils.GetNowString()
+
 	usersDB[user.Id] = user
 	return nil
 }
