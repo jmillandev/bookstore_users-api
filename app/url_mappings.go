3cbf3bb685d63ffe5dd00ee49c182451951a1910
Jesus Millan <millan@asistesi.com>
2021-09-05 19:45:18 -0400

    imp: Version API

--- app/url_mappings.go
+++ app/url_mappings.go
@@ -8,11 +8,11 @@ import (
 func mapUrls() {
 	router.GET("/ping/", ping.Ping)
 
-	router.POST("/users/", users.Create)
-	router.GET("/users/:user_id/", users.Get)
-	router.PUT("/users/:user_id/", users.Update)
-	router.PATCH("/users/:user_id/", users.Update)
-	router.DELETE("/users/:user_id/", users.Delete)
-	router.POST("/login/", users.Login)
-	router.GET("/internal/users/search/", users.Search)
+	router.POST("/v1/users/", users.Create)
+	router.GET("/v1/users/:user_id/", users.Get)
+	router.PUT("/v1/users/:user_id/", users.Update)
+	router.PATCH("/v1/users/:user_id/", users.Update)
+	router.DELETE("/v1/users/:user_id/", users.Delete)
+	router.POST("/v1/login/", users.Login)
+	router.GET("/v1/internal/users/search/", users.Search)
 }
