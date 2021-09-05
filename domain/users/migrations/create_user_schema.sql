e755614bcace2cea97351ee1b3ed9cc5b61e7425
Jesus Millan <millan@asistesi.com>
2021-09-05 12:53:16 -0400

    feat: Create init migrations for users

---
+++ domain/users/migrations/create_user_schema.sql
@@ -0,0 +1,10 @@
+CREATE TABLE users(
+    id INT UNSIGNED AUTO_INCREMENT,
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
+    email VARCHAR(50) NOT NULL UNIQUE,
+    status VARCHAR(10) NOT NULL,
+    password VARCHAR(250) NOT NULL,
+    date_created TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY(id)
+);
