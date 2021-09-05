924ee92674bd2a3d193dacc13f59d365fd1dc205
Jesus Millan <millan@asistesi.com>
2021-09-05 12:21:00 -0400

    feat:Dockerize Database

--- datasources/mysql/users_db/users_db.go
+++ datasources/mysql/users_db/users_db.go
@@ -7,21 +7,33 @@ import (
 	"os"
 
 	_ "github.com/go-sql-driver/mysql"
+	"github.com/joho/godotenv"
 )
 
 var (
-	mysql_username = os.Getenv("DB_USERNAME")
-	mysql_password = os.Getenv("DB_PASSWORD")
-	mysql_host     = os.Getenv("DB_HOST")
-	mysql_port     = os.Getenv("DB_PORT")
-	mysql_schema   = os.Getenv("DB_NAME")
+	mysql_schema, mysql_host, mysql_password, mysql_username, mysql_port string
 )
 
 var (
 	Client *sql.DB
 )
 
+func set_mysql_config() {
+	err := godotenv.Load(".env")
+
+	if err != nil {
+		log.Fatalf("Error loading .env file")
+	}
+
+	mysql_username = os.Getenv("MYSQL_USER")
+	mysql_password = os.Getenv("MYSQL_PASSWORD")
+	mysql_host = os.Getenv("MWSQL_HOST")
+	mysql_port = os.Getenv("MYSQL_PORT")
+	mysql_schema = os.Getenv("MYSQL_DATABASE")
+}
+
 func init() {
+	set_mysql_config()
 	dataSourceName := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8",
 		mysql_username, mysql_password, mysql_host, mysql_port, mysql_schema,
 	)
